65666cb577019a796c9d6f8bf3c71514c34b0808
rodfer0x80 <rodrigolf080@gmail.com>
2024-03-28 00:10:09 +0000

    tis works without docker now

--- assembler.sh
+++ assembler.sh
@@ -18,23 +18,8 @@ aarch64-linux-gnu-as -o "$OBJ_FILE" "$ASM_FILE"
 aarch64-linux-gnu-gcc -o "$ARM64_BIN"  "$OBJ_FILE"
 rm "$OBJ_FILE"
 
-# Build the Docker image
-BUILDER_NAME="charm64"
-# Check if the builder already exists
-if docker buildx inspect "$BUILDER_NAME" &> /dev/null; then
-    echo "[*] Builder '$BUILDER_NAME' already exists."
-else
-    # Create the builder
-    docker buildx create --name "$BUILDER_NAME" --use
-    echo "[+] Builder '$BUILDER_NAME' created and set as active."
-fi
-
-# Build the Docker image
-docker buildx build --load --platform linux/arm64 -t charm64 -f Dockerfile .
-rm "$ARM64_BIN"
-
-# Run the Docker container
-docker run --platform linux/arm64 charm64
+# Run with qemu
+qemu-aarch64 "$ARM64_BIN"
 
 # Remove runme elf arm64 executable
 #rm runme.arm64
